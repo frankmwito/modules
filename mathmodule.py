5288c016701f26b6089d7e2b76aff9ed3bc49706
Frank Mwito <Mfranklyne039@gmail.com>
2024-09-02 18:47:04 +0300

    square root using math module

---
+++ mathmodule.py
@@ -0,0 +1,8 @@
+# calculate the square root of a given number
+
+import math
+
+def square():
+    num = float(input("Enter a number: "))
+    return math.sqrt(num)
+print("The square root is", square())
